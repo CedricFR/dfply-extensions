459ed4534531bff0a41e2fbf8c013ce50868e95d
Cedric <20124295+CedricFR@users.noreply.github.com>
2018-01-13 16:22:36 +0100

    Initial setup commit

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'dfply-extensions',
+    version = '0.0.1',
+    author = 'Cedric Canovas',
+    keywords = 'pandas dplyr plotly',
+    packages = find_packages(),
+    include_package_data=True,
+    package_dir={'dfply-extensions':'src'},
+    install_requires=['numpy', 'pandas', 'dfply'],
+    description = 'additional functions for dfply',
+    long_description = 'See https://github.com/cedricfr/dfply-extensions/blob/master/README.md for details.',
+    license = 'Apache License v2.0',
+    url = 'https://github.com/cedricfr/dfply-extensions',
+    test_suite='test',
+)
